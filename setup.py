717d60a176a93240055ccf1b05497ddbfb554f5e
Sribarrow <sribarrow@gmail.com>
2022-02-13 20:05:38 +0000

    updated url

--- setup.py
+++ setup.py
@@ -11,9 +11,9 @@ setuptools.setup(
     description="A small example package",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/pypa/sampleproject",
+    url="https://github.com/sribarrow/tdd_proj_mt",
     project_urls={
-        "Bug Tracker": "https://github.com/pypa/sampleproject/issues",
+        "Bug Tracker": "https://github.com/sribarrow/tdd_proj_mt/issues",
     },
     classifiers=[
         "Programming Language :: Python :: 3",
